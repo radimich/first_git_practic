cd1e03fd040722c5015ad0a85c2254c824990348
radimich <radimich25@yandex.ru>
2020-01-27 23:10:01 +0300

    first commit

---
+++ Practic for git.py
@@ -0,0 +1,9 @@
+#-------------------------------------
+#    Program by Smolyak EW
+#
+#   Version   Date   Information
+#     1.0     2019     13/06
+#
+#-------------------------------------
+
+import numpy
